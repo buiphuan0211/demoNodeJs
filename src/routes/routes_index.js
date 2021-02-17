2d614b540e5fa897fe6ec87d3193872d866dc85c
AnBui <buiphuan021100@gmail.com>
2021-02-17 12:16:40 +0700

    upload file

---
+++ src/routes/routes_index.js
@@ -0,0 +1,10 @@
+const newsRoutes = require("./news");
+const siteRoutes = require("./site");
+
+function routes(app) {
+  app.use("/news", newsRoutes);
+
+  app.use("/", siteRoutes);
+}
+
+module.exports = routes;
